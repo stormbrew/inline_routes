43944f37092aa6ec3484c3a2a709b5a219cdc337
graham@graham-battys-macbook.local <graham@graham-battys-macbook.local>
2009-02-15 19:17:53 -0700

    Made it autoreload even the inline routes by modifying the update-checking to look at the maximum mtime of all of the files affected together (routes.rb and all of the controllers). This could get a lot smarter (eg. not look at controllers that didn't define any inline routes), but it works for now.

--- lib/inline_routes.rb
+++ lib/inline_routes.rb
@@ -1,3 +1,17 @@
+class File
+	def self.stat_load_path(filename)
+		file_stat = nil
+		$:.each {|path|
+			begin
+				file_stat = File.stat("#{path}/#{filename}")
+				break
+			rescue
+			end
+		}
+		return file_stat || raise(Errno::ENOENT)
+	end
+end
+
 module InlineRoutes
 	class Mapper
 		attr :real_mapper
@@ -43,6 +57,7 @@ module ::ActionController
 						begin
 							Thread.current[:inline_routes_search_map] = InlineRoutes::Mapper.new(self, controller)
 							catch(:done_inline_routes) {
+								Thread.current[:inline_routes_mtime_list].push(File.stat_load_path("#{controller}_controller.rb").mtime) if Thread.current[:inline_routes_mtime_list]
 								load("#{controller}_controller.rb")
 							}
 						ensure
@@ -52,6 +67,30 @@ module ::ActionController
 				end
 			end
 			
+			def reload
+        if @routes_last_modified && configuration_file
+          mtimes = [File.stat(configuration_file).mtime]
+					mtimes += ::ActionController::Routing.possible_controllers.collect {|controller| File.stat_load_path("#{controller}_controller.rb").mtime }
+					mtime = mtimes.max
+          # if it hasn't been changed, then just return
+          return if mtime <= @routes_last_modified
+          # if it has changed then record the new time and fall to the load! below
+          @routes_last_modified = mtime
+        end
+        load!
+			end
+			
+			alias_method(:inline_routes_original_load_routes!, :load_routes!)
+			def load_routes!
+				begin
+					Thread.current[:inline_routes_mtime_list] = []
+					inline_routes_original_load_routes!
+					@routes_last_modified = [@routes_last_modified, *Thread.current[:inline_routes_mtime_list]].max
+				ensure
+					Thread.current[:inline_routes_mtime_list] = nil
+				end
+			end
+
 			def inline_draw(&block)
 				# Do inline routes if the tls block is set
 				if (Thread.current[:inline_routes_search_map])
